7aca47685903deb2f22ab5b824a8f08e2b74efd9
shirleyvirtual <shir.copias@gmail.com>
2025-10-27 21:28:01 -0400

    configuracion de seeder empleado y database

--- laravel/empleados/database/seeders/EmpleadoSeeder.php
+++ laravel/empleados/database/seeders/EmpleadoSeeder.php
@@ -2,16 +2,38 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EmpleadoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('empleados')->insert([
+            [
+                'nombre' => 'Shirley',
+                'apellido' => 'Lima',
+                'correo' => 'shirley.lima@gmail.com',
+                'salario' => 5000.00,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nombre' => 'María',
+                'apellido' => 'Carrillo',
+                'correo' => 'mariac@gmail.com',
+                'salario' => 4500,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nombre' => 'Carlos',
+                'apellido' => 'López',
+                'correo' => 'carlos.lopez@gmail.com',
+                'salario' => 3000,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
